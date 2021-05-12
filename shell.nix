81f3ebb47784e6744f144159a250b6e89a53f8e4
Antonio Yang <yanganto@gmail.com>
2021-05-12 22:52:37 +0800

    Init ssvm test environment

--- shell.nix
+++ shell.nix
@@ -5,22 +5,23 @@ let
       rev = "57c8084c7ef41366993909c20491e359bbb90f54";
     });
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
-  rust = with nixpkgs; ((rustChannelOf { channel = "stable"; }).rust.override {
+  rust = with nixpkgs; ((rustChannelOf { date = "2021-05-06"; channel = "stable"; }).rust.override {
     extensions = [ "rust-src" ];
   });
+
+  clangStdenv = nixpkgs.llvmPackages_10.stdenv;
 in
-with nixpkgs; pkgs.mkShell {
-  buildInputs = [
-    clang
+clangStdenv.mkDerivation {
+  name = "clang-10-nix-shell";
+  buildInputs = with nixpkgs; [
     cmake
     pkg-config
     rust
+
+    llvmPackages_10.llvm
+    lld_10
     boost
   ] ++ stdenv.lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
   ];
-
-  LIBCLANG_PATH = "${llvmPackages.libclang}/lib";
-  PROTOC = "${protobuf}/bin/protoc";
-  ROCKSDB_LIB_DIR = "${rocksdb}/lib";
 }
