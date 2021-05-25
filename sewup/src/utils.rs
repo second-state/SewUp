c607b34c3d2cf1dfcbba7003b1dffe4a502c9243
Antonio Yang <yanganto@gmail.com>
2021-05-25 16:06:22 +0800

    Add utils for function signature

--- sewup/src/utils.rs
+++ sewup/src/utils.rs
@@ -1,4 +1,4 @@
-use tiny_keccak::{Hasher, Sha3};
+use tiny_keccak::{Hasher, Keccak, Sha3};
 
 pub fn copy_into_array<A, T>(slice: &[T]) -> A
 where
@@ -16,4 +16,25 @@ pub fn sha3_256(input: &[u8]) -> [u8; 32] {
     hasher.update(input);
     hasher.finalize(&mut output);
     output
+}
+
+pub fn get_function_signature(funtion_prototype: &str) -> [u8; 4] {
+    let mut sig = [0; 4];
+    let mut hasher = Keccak::v256();
+    hasher.update(funtion_prototype.as_bytes());
+    hasher.finalize(&mut sig);
+    sig
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex_literal::*;
+    #[test]
+    fn test_function_signature() {
+        let mut sig: [u8; 4] = hex!("c48d6d5e");
+        assert_eq!(get_function_signature("sendMessage(string,address)"), sig);
+        sig = hex!("70a08231");
+        assert_eq!(get_function_signature("balanceOf(address)"), sig);
+    }
 }
