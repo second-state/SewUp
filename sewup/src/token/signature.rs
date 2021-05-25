d7e8f9899c2187105c3579b185f4046ab364f514
Antonio Yang <yanganto@gmail.com>
2021-05-25 17:57:25 +0800

    Add utils for function signature (#29)

--- sewup/src/token/signature.rs
+++ sewup/src/token/signature.rs
@@ -1,29 +1,51 @@
-// 0x9993021a do_balance() ABI signature
 pub const DO_BALANCE_SIGNATURE: [u8; 4] = [153, 147, 2, 26];
 
-// 0x5d359fbd do_transfer() ABI signature
 pub const DO_TRANSFER_SIGNATURE: [u8; 4] = [93, 53, 159, 189];
 
-// 0x06fdde03 name() ABI signature
 pub const NAME_SIGNATURE: [u8; 4] = [6, 253, 222, 3];
 
-// 0x95d89b41 symbol() ABI signature
 pub const SYMBOL_SIGNATURE: [u8; 4] = [149, 216, 155, 65];
 
-// 0x313ce567 decimals() ABI signature
 pub const DEVIMALS_SIGNATURE: [u8; 4] = [49, 60, 229, 103];
 
-// 0x18160ddd totalSupply() ABI signature
 pub const TOTAL_SUPPLY_SIGNATURE: [u8; 4] = [24, 22, 13, 221];
 
-// 0x1086a9aa approve(address, uint64) ABI signature
 pub const APPROVE_SIGNATURE: [u8; 4] = [16, 134, 169, 170];
 
-// 0xdd62ed3e allowance(address, address) ABI signature
 pub const ALLOWANCE_SIGNATURE: [u8; 4] = [221, 98, 237, 62];
 
-// 0x2ea0dfe1 transferFrom(address, address, uint64) ABI signature
 pub const TRANSFER_FROM_SIGNATURE: [u8; 4] = [46, 160, 223, 225];
 
 // 0xaa174ccb mint _mint(address account, uint256 amount)
 pub const MINT_SIGNATURE: [u8; 4] = [170, 23, 76, 203];
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::get_function_signature;
+    #[test]
+    fn test_function_signature() {
+        // assert_eq!(get_function_signature("doBalance(address)"), DO_BALANCE_SIGNATURE);
+        // assert_eq!(get_function_signature("doTransfer(address,value)"), DO_TRANSFER_SIGNATURE);
+        assert_eq!(get_function_signature("name()"), NAME_SIGNATURE);
+        assert_eq!(get_function_signature("symbol()"), SYMBOL_SIGNATURE);
+        assert_eq!(get_function_signature("decimals()"), DEVIMALS_SIGNATURE);
+        assert_eq!(
+            get_function_signature("totalSupply()"),
+            TOTAL_SUPPLY_SIGNATURE
+        );
+        assert_eq!(
+            get_function_signature("approve(address,uint64)"),
+            APPROVE_SIGNATURE
+        );
+        assert_eq!(
+            get_function_signature("allowance(address,address)"),
+            ALLOWANCE_SIGNATURE
+        );
+        assert_eq!(
+            get_function_signature("transferFrom(address,address,uint64)"),
+            TRANSFER_FROM_SIGNATURE
+        );
+        // assert_eq!(get_function_signature("mint(address,uint256)"), MINT_SIGNATURE);
+    }
+}
