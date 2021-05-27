9ac013e9cb87dafeb51e070438290c23edeb9ac7
Antonio Yang <yanganto@gmail.com>
2021-05-27 16:21:33 +0800

    Fix CI (#32)
    * Use default llvm

--- shell.nix
+++ shell.nix
@@ -5,40 +5,35 @@ let
       rev = "57c8084c7ef41366993909c20491e359bbb90f54";
     });
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
-  rust = with nixpkgs; ((rustChannelOf { date = "2021-05-06"; channel = "stable"; }).rust.override {
+  rust-stable = with nixpkgs; ((rustChannelOf { date = "2021-05-06"; channel = "stable"; }).rust.override {
     targets = [ "wasm32-unknown-unknown" ];
   });
-
-  updateConteact = nixpkgs.writeShellScriptBin "update-contract" ''
+  updateContract = nixpkgs.writeShellScriptBin "update-contract" ''
     cd erc20-contract
     cargo build --release
     cd ../
     mv target/wasm32-unknown-unknown/release/erc20_contract.wasm resources/test/erc20_contract.wasm
   '';
   testScript = nixpkgs.writeShellScriptBin "run-test" ''
+    cd sewup
     cargo test -p sewup --features=$1 -- --nocapture | tee /tmp/vm_errors && exit $(grep ERROR /tmp/vm_errors | wc -l)
+    cd ../
   '';
-  clangStdenv = nixpkgs.llvmPackages_10.stdenv;
 in
-clangStdenv.mkDerivation {
-  name = "clang-10-nix-shell";
-  buildInputs = with nixpkgs; [
+with nixpkgs; pkgs.mkShell {
+  buildInputs = [
+    clang
     cmake
     pkg-config
-    rust
-    clippy
-
-    llvmPackages_10.llvm
-    lld_10
+    rust-stable
     boost
-
+    updateContract
     testScript
-    updateConteact
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
   ];
-  nativeBuildInputs = with nixpkgs; [
-    cmake
-  ];
-  LIBCLANG_PATH = "${nixpkgs.llvmPackages_10.libclang}/lib";
+
+  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  PROTOC = "${protobuf}/bin/protoc";
+  ROCKSDB_LIB_DIR = "${rocksdb}/lib";
 }
