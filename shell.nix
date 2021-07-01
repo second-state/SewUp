089dcb4ce6926c6e08d635e21f3f246165bdd8d0
Antonio Yang <yanganto@gmail.com>
2021-07-01 09:23:45 +0800

    run example in parallel (#72)

--- shell.nix
+++ shell.nix
@@ -8,23 +8,6 @@ let
   rust-stable = with nixpkgs; ((rustChannelOf { date = "2021-05-06"; channel = "stable"; }).rust.override {
     targets = [ "wasm32-unknown-unknown" ];
   });
-  updateContract = nixpkgs.writeShellScriptBin "update-contract" ''
-    update-single-contract erc20
-  '';
-  updateSingleContract = nixpkgs.writeShellScriptBin "update-single-contract" ''
-    rm -f resources/test/$1_contract.wasm
-    cd examples/$1-contract
-    cargo build --release --target=wasm32-unknown-unknown
-    cd ../../
-    mv examples/$1-contract/target/wasm32-unknown-unknown/release/$1_contract.wasm resources/test/$1_contract.wasm \
-      & echo "==> update $1" \
-      & echo "==> `ls -l resources/test/$1_contract.wasm`"
-  '';
-  testScript = nixpkgs.writeShellScriptBin "run-test" ''
-    cd sewup
-    cargo test -p sewup --no-default-features --features=$1 -- --nocapture | tee /tmp/vm_errors && exit $(grep ERROR /tmp/vm_errors | wc -l)
-    cd ../
-  '';
   exampleTestScript = nixpkgs.writeShellScriptBin "run-example-test" ''
     cd examples/$1-contract
     cargo test
@@ -38,9 +21,6 @@ with nixpkgs; pkgs.mkShell {
     pkg-config
     rust-stable
     boost
-    updateContract
-    updateSingleContract
-    testScript
     exampleTestScript
   ] ++ lib.optionals stdenv.isDarwin [
     darwin.apple_sdk.frameworks.Security
