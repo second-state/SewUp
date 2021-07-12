1448f1e7fc1df40c7260f64d22019a447404fb65
Antonio Yang <yanganto@gmail.com>
2021-07-12 15:51:28 +0800

    Add tests for fn-sig

--- sewup-derive/tests/compile-pass/ewasm-fn-sig.rs
+++
@@ -1,8 +0,0 @@
-use sewup_derive::ewasm_fn_sig;
-
-const SOME_HANDLE_SIG: [u8; 4] = [0, 0, 0, 0];
-
-fn main() {
-    let _a = ewasm_fn_sig!(some_handle);
-    let _b = ewasm_fn_sig!(other_handle());
-}
