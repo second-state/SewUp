5dd060135c8941c185a2ea4607adc482cf173b2a
Antonio Yang <yanganto@gmail.com>
2021-07-13 16:42:54 +0800

    Refactor ewasm_fn_sig!

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ let
       rev = "57c8084c7ef41366993909c20491e359bbb90f54";
     });
   nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
-  rust-stable = with nixpkgs; ((rustChannelOf { date = "2021-05-06"; channel = "stable"; }).rust.override {
+  rust-stable = with nixpkgs; ((rustChannelOf { date = "2021-05-14"; channel = "nightly"; }).rust.override {
     targets = [ "wasm32-unknown-unknown" ];
   });
   exampleTestScript = nixpkgs.writeShellScriptBin "run-example-test" ''
