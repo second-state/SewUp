8d310b550419ec0566186a61604552f4aa55a133
yanganto <yanganto@users.noreply.github.com>
2021-07-23 07:09:03 +0000

    Automated deployment: Fri Jul 23 07:09:03 UTC 2021 ecebe8cadf9c234e4e9cfb0893db8ef6178bca43

--- sewup/primitives/sidebar-items.js
+++ sewup/primitives/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Contract",""]],"type":[["FunctionSignature",""]]});
+initSidebarItems({"struct":[["Contract",""],["EwasmAny","helps you return different type of date in the contract handlers The any serializable data can easy to become EwasmAny by following command `EwasmAny::from(protocol)` and the data will preserialized and store in the EwasmAny structure, once the `ewasm_main` function try to retrun the instance of EwasmAny, the preserialized data will be returned."]],"trait":[["IntoEwasmAny",""]],"type":[["FunctionSignature",""]]});
