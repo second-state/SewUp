8122577c360f8057f232b359ddcd81ec2c91f7d1
Antonio Yang <yanganto@gmail.com>
2021-08-05 10:14:59 +0800

    Refactor contract handler

--- sewup/src/runtimes/handler.rs
+++ sewup/src/runtimes/handler.rs
@@ -2,8 +2,7 @@
 //! The handler helps you deploy contract and test the contract you developing
 use std::cell::RefCell;
 use std::fmt;
-use std::fs::{self, read};
-use std::path::PathBuf;
+use std::fs::read;
 use std::sync::Arc;
 
 use crate::errors::ContractError as Error;
@@ -15,24 +14,9 @@ use serde_derive::{Deserialize, Serialize};
 
 #[derive(Clone, Deserialize, Serialize, Default)]
 pub struct ContractHandler {
-    /// The contract address, when the contract is not deployed,
-    /// the `connect` function called the contract will be automatically
-    /// deployed and the address field of the config will be filled
-    pub contract_address: Option<Address>,
-
-    /// The address of the sender, who calls the contract
-    #[serde(skip)]
-    pub sender_address: Address,
-
     /// The contract data in hex literal for eWasm binary, or a file path to
     /// the .ewasm file
     pub call_data: Option<String>,
-
-    /// If the contract_config_file_path pass from the connection string,
-    /// this field will be filled
-    #[serde(skip)]
-    pub config_file_path: Option<PathBuf>,
-
     #[serde(skip)]
     pub rt: Option<Arc<RefCell<dyn RT>>>,
 }
@@ -40,56 +24,13 @@ pub struct ContractHandler {
 impl fmt::Debug for ContractHandler {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("ContractHandler")
-            .field("contract_address", &self.contract_address)
-            .field("sender_address", &self.sender_address)
             .field("call_data", &self.call_data)
-            .field("config_file_path", &self.config_file_path)
             .field("rt", &self.rt.is_some())
             .finish()
     }
 }
 
 impl ContractHandler {
-    /// Reach the contract, if the contract is not exist, then deploy it first
-    pub fn connect(&mut self, gas: i64) -> Result<()> {
-        if self.contract_address.is_none() {
-            return self.deploy(gas);
-        }
-        Ok(())
-    }
-
-    /// deploy the contract from call data store contract address into config
-    fn deploy(&mut self, gas: i64) -> Result<()> {
-        if let Some(config_file_path) = self.config_file_path.take() {
-            if let Some(call_data) = self.call_data.take() {
-                let call_data = ContractHandler::get_call_data(call_data)?;
-                if call_data.len() < 4 {
-                    return Err(Error::ContractSizeError(call_data.len()).into());
-                }
-                if let Some(rt) = self.rt.take() {
-                    let msg = VMMessageBuilder {
-                        sender: Some(&self.sender_address),
-                        input_data: Some(&call_data),
-                        gas,
-                        ..Default::default()
-                    }
-                    .build()?;
-                    self.contract_address = Some(*rt.borrow_mut().deploy(msg)?);
-                    self.rt = Some(rt);
-                    fs::write(
-                        config_file_path,
-                        toml::to_string(self).expect("config generate error"),
-                    )
-                    .expect("config file can not be updated");
-                    return Ok(());
-                }
-                panic!("rt should be init when parsing the connection string")
-            }
-            return Err(Error::InsufficientContractInfoError.into());
-        }
-        panic!("config_file_path should be update when ContractHandler constructed")
-    }
-
     pub fn execute(
         &mut self,
         fun_sig: [u8; 4],
@@ -104,9 +45,9 @@ impl ContractHandler {
                 if let Some(input) = input {
                     input_data.extend_from_slice(input);
                 }
-
+                let sender = Address::default();
                 let msg = VMMessageBuilder {
-                    sender: Some(&self.sender_address),
+                    sender: Some(&sender),
                     input_data: Some(&input_data),
                     gas,
                     code: Some(&call_data),
