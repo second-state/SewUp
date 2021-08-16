70d8c2d15da3e7126aee35f420555d3ef60debe4
Antonio Yang <yanganto@gmail.com>
2021-08-16 14:22:06 +0800

    Init ERC-1155

---
+++ examples/erc1155-contract/src/token.rs
@@ -0,0 +1,18 @@
+use sewup_derive::{ewasm_constructor, ewasm_fn, ewasm_fn_sig, ewasm_main, ewasm_test};
+
+#[ewasm_constructor]
+fn constructor() {}
+
+#[ewasm_main]
+fn main() -> anyhow::Result<()> {
+    let contract = sewup::primitives::Contract::new()?;
+    match contract.get_function_selector()? {
+        _ => (),
+    };
+    Ok(())
+}
+
+#[ewasm_test]
+mod tests {
+    use super::*;
+}
---
+++ sewup/src/token/erc1155.rs
@@ -0,0 +1,10 @@
+// isApprovedForAll(address,address): e985e9c5
+// setApprovalForAll(address,bool): a22cb465
+// balanceOfBatch(address,uint256): 830e3ae4
+// balanceOf(address,uint256): 00fdd58e
+// safeBatchTransferFrom(address,address,uint256[],uint256[],bytes): 2eb2c2d6
+// safeTransferFrom(address,address,uint256,uint256,bytes): f242432a
+// TransferSingle(address,address,address,uint256,uint256): c3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62
+// TransferBatch(address,address,address,uint256[],uint256[]): 4a39dc06d4c0dbc64b70af90fd698a233a518aa5d07e595d983b8c0526c8f7fb
+// ApprovalForAll(address,address,bool): 17307eab39ab6107e8899845ad3d59bd9653f200f220920489ca2b5937696c31
+// URI(string,uint256): 6bb7ff708619ba0610cba295a58592e0451dee2622938c8755667688daf3529b
