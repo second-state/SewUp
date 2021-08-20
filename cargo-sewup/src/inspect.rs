88b008a40538532ffa0e9d9bf7e6cf91a68b1ea3
Antonio Yang <yanganto@gmail.com>
2021-08-20 17:11:28 +0800

    Remove wasm2wat in inspector of cli

--- cargo-sewup/src/inspect.rs
+++ cargo-sewup/src/inspect.rs
@@ -1,11 +1,9 @@
-use std::env;
+use std::collections::HashSet;
 
 use anyhow::{Context, Result};
 use hex::decode;
-use tokio::{
-    fs::{read_to_string, write},
-    process::Command,
-};
+use tokio::fs::read_to_string;
+use wasmparser::{Parser, Payload};
 
 pub async fn run(inspect_file: String) -> Result<()> {
     let mut deploy_content = read_to_string(inspect_file)
@@ -14,22 +12,38 @@ pub async fn run(inspect_file: String) -> Result<()> {
 
     deploy_content = deploy_content.trim_end().to_string();
 
-    let wasm_bin = decode(&deploy_content).context("fail to decode deploy file to binary")?;
+    let mut exports = HashSet::<String>::new();
 
-    let temp_wasm_path = env::temp_dir().join("inspect.wasm");
-
-    write(temp_wasm_path.clone(), wasm_bin)
-        .await
-        .context("fail to generate hex string for deploy wasm")?;
-
-    let output = Command::new("wasm2wat")
-        .args(&[temp_wasm_path])
-        .output()
-        .await
-        .context("fail to build wasm")?;
-    let tmpl = String::from_utf8_lossy(&output.stdout);
-
-    println!("{}", tmpl);
-
+    let buf: Vec<u8> = decode(&deploy_content).context("fail to decode deploy file to binary")?;
+    for payload in Parser::new(0).parse_all(&buf) {
+        match payload? {
+            Payload::Version { num, .. } => {
+                println!("====== Module version: {}", num);
+            }
+            Payload::ExportSection(s) => {
+                for export in s {
+                    let export = export?;
+                    println!("  Export {} {:?}", export.field, export.kind);
+                    exports.insert(format!("{} {:?}", export.field, export.kind));
+                }
+            }
+            Payload::ImportSection(s) => {
+                for import in s {
+                    let import = import?;
+                    println!("  Import {}::{}", import.module, import.field.unwrap());
+                }
+            }
+            _other => {
+                println!("  {:?}", _other);
+            }
+        }
+    }
+    if exports.len() != 2
+        || !exports.contains("memory Memory")
+        || !exports.contains("main Function")
+    {
+        println!("====== Result");
+        println!("Ethereum wasm only export memory and main function");
+    }
     Ok(())
 }
