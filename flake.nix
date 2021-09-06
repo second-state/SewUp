ee4465000d7041e27d4d85b304d58ee51e5c1ace
Antonio Yang <yanganto@gmail.com>
2021-09-06 10:45:06 +0800

    Update CI with flake

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        devRustNightly = pkgs.rust-bin.nightly."2021-08-31".default.override {
+          extensions = [ "rust-src" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        exampleTestScript = pkgs.writeShellScriptBin "run-example-test" ''
+          cd examples/$1-contract
+          cargo test
+          rc=$?
+          cd ../../
+          exit $rc
+        '';
+        cliTestScript = pkgs.writeShellScriptBin "cli-test" ''
+          cd cargo-sewup
+          cargo run -- -d -b -p ../examples/$1-contract
+          cd ../
+          ls -l examples/$1-contract/target/wasm32-unknown-unknown/release/$1_contract.deploy
+          rc=$?
+          exit $rc
+        '';
+        abiTestScript = pkgs.writeShellScriptBin "abi-test" ''
+          cd cargo-sewup
+          cargo run -- -g -p ../examples/$1-contract | jq --sort-keys > /tmp/$1_abi.json
+          cd ../
+          diff /tmp/$1_abi.json asset/$1_abi.json
+          rc=$?
+          exit $rc
+        '';
+        clientTestScript = pkgs.writeShellScriptBin "run-client-test" ''
+          cd examples/$1-contract
+          cargo build --bin $1-client --features=client
+          rc=$?
+          cd ../../
+          exit $rc
+        '';
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            boost
+            clang
+            cmake
+            openssl
+            pkg-config
+            devRustNightly
+            exampleTestScript
+            cliTestScript
+            abiTestScript
+            clientTestScript
+          ];
+
+          LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+          PROTOC = "${protobuf}/bin/protoc";
+          ROCKSDB_LIB_DIR = "${rocksdb}/lib";
+        };
+      }
+    );
+}
