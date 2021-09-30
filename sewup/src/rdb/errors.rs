cd2edb9f8b851c4997a66745e2f639713c423a42
Antonio Yang <yanganto@gmail.com>
2021-09-30 08:27:29 +0800

    Sorted errors (#204)

--- sewup/src/rdb/errors.rs
+++ sewup/src/rdb/errors.rs
@@ -1,15 +1,16 @@
 use thiserror::Error;
 
+#[remain::sorted]
 #[derive(Error, Debug, PartialEq)]
 pub enum Error {
-    #[error("`{0}` did not exist")]
-    TableNotExist(String),
-    #[error("No record in table")]
-    TableIsEmpty,
-    #[error("Record is not fixed, and overflowed")]
-    RecordNotSized,
-    #[error("Record Id not correct, it starts from 1 not zero")]
-    RecordIdCorrect,
     #[error("Record deleted")]
     RecordDeleted,
+    #[error("Record Id not correct, it starts from 1 not zero")]
+    RecordIdCorrect,
+    #[error("Record is not fixed, and overflowed")]
+    RecordNotSized,
+    #[error("No record in table")]
+    TableIsEmpty,
+    #[error("`{0}` did not exist")]
+    TableNotExist(String),
 }
