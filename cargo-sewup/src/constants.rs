2e289774ec1881d201e8a9a44e1d7c5a123e2b63
Antonio Yang <yanganto@gmail.com>
2021-10-01 11:35:06 +0800

    Init Rusty mode project

--- cargo-sewup/src/constants.rs
+++ cargo-sewup/src/constants.rs
@@ -7,3 +7,150 @@ macro_rules! deploy_wasm {
 
 pub const DEFAULT_GAS: usize = 500_000_000;
 pub const DEFAULT_GAS_PRICE: usize = 1;
+
+#[macro_export]
+macro_rules! default_cargo_template {
+    () => {
+        r#"
+[package]
+name = "{}"
+version = "0.1.0"
+edition = "2018"
+
+[lib]
+path = "src/lib.rs"
+crate-type = ["cdylib"]
+
+# See the following examples
+# https://github.com/second-state/SewUp/tree/main/examples/hello-contract
+# https://github.com/second-state/SewUp/tree/main/examples/default-contract
+
+[dependencies]
+sewup = "*"
+sewup-derive = "*"
+anyhow = "*"
+# thiserror = "*"
+
+[profile.release]
+incremental = false
+panic = "abort"
+lto = true
+opt-level = "z"
+
+[profile.release.package.{}]
+incremental = false
+opt-level = "z"
+
+[features]
+constructor = []
+constructor-test = []"#
+    };
+}
+
+pub const DEFAULT_CONTRACT: &str = r#"
+use sewup_derive::{ewasm_constructor, ewasm_fn, ewasm_fn_sig, ewasm_main, ewasm_test};
+
+#[ewasm_constructor]
+fn constructor() {}
+
+#[ewasm_fn]
+fn hello() -> anyhow::Result<String> {
+    Ok("hello world".to_string())
+}
+
+#[ewasm_main(auto)]
+fn main() -> anyhow::Result<String> {
+    let contract = sewup::primitives::Contract::new()?;
+    let greeting = match contract.get_function_selector()? {
+        ewasm_fn_sig!(hello) => hello()?,
+        _ => panic!("unknown handle"),
+    };
+    Ok(greeting)
+}
+
+#[ewasm_test]
+mod tests {
+    use super::*;
+    use sewup_derive::{ewasm_assert_eq, ewasm_auto_assert_eq, ewasm_output_from};
+
+    #[ewasm_test]
+    fn test_get_greeting() {
+        ewasm_auto_assert_eq!(hello(), "hello world".to_string());
+    }
+}"#;
+
+#[macro_export]
+macro_rules! rusty_cargo_template {
+    () => {
+        r#"
+[package]
+name = "{}"
+version = "0.1.0"
+edition = "2018"
+
+[lib]
+path = "src/lib.rs"
+crate-type = ["cdylib"]
+
+# See the following examples
+# https://github.com/second-state/SewUp/tree/main/examples/rusty-contract
+
+[dependencies]
+sewup = "*"
+sewup-derive = "*"
+
+[profile.release]
+incremental = false
+panic = "abort"
+lto = true
+opt-level = "z"
+
+[profile.release.package.{}]
+incremental = false
+opt-level = "z"
+
+[features]
+constructor = []
+constructor-test = []"#
+    };
+}
+
+pub const RUSTY_CONTRACT: &str = r#"
+use sewup_derive::{ewasm_constructor, ewasm_fn, ewasm_fn_sig, ewasm_main, ewasm_test};
+
+#[ewasm_constructor]
+fn constructor() {}
+
+#[ewasm_fn]
+fn handler() -> Result<(), &'static str> {
+    Ok(())
+}
+
+#[ewasm_main(rusty)]
+fn main() -> Result<(), &'static str> {
+    use sewup::primitives::Contract;
+    use sewup_derive::ewasm_input_from;
+
+    let contract = Contract::new().map_err(|_| "NewContractError")?;
+    match contract
+        .get_function_selector()
+        .map_err(|_| "FailGetFnSelector")?
+    {
+        ewasm_fn_sig!(handler) => handler()?,
+        _ => return Err("UnknownHandle"),
+    };
+
+    Ok(())
+}
+
+#[ewasm_test]
+mod tests {
+    use super::*;
+    use sewup::primitives::Contract;
+    use sewup_derive::{ewasm_assert_eq, ewasm_rusty_assert_ok, ewasm_rusty_err_output};
+
+    #[ewasm_test]
+    fn test_handler_ok() {
+        ewasm_rusty_assert_ok!(handler());
+    }
+}"#;
--- cargo-sewup/src/init.rs
+++ cargo-sewup/src/init.rs
@@ -4,6 +4,11 @@ use anyhow::{Context, Result};
 use clap::arg_enum;
 use tokio::fs::{create_dir, write};
 
+use cargo_sewup::{
+    constants::{DEFAULT_CONTRACT, RUSTY_CONTRACT},
+    default_cargo_template, rusty_cargo_template,
+};
+
 arg_enum! {
     pub enum Mode {
         Default,
@@ -59,45 +64,13 @@ async fn init_cargo_toml(mode: &Mode) -> Result<()> {
     let current_folder = std::env::current_dir()?;
     let project_name = current_folder.file_name().unwrap().to_string_lossy();
 
-    write(
-        "Cargo.toml",
-        format!(
-            r#"
-[package]
-name = "{}"
-version = "0.1.0"
-edition = "2018"
-
-[lib]
-path = "src/lib.rs"
-crate-type = ["cdylib"]
-
-# See the hello example if you want to use a rust client
-# https://github.com/second-state/SewUp/tree/main/examples/hello-contract
-
-[dependencies]
-sewup = "*"
-sewup-derive = "*"
-anyhow = "*"
-
-[profile.release]
-incremental = false
-panic = "abort"
-lto = true
-opt-level = "z"
-
-[profile.release.package.{}]
-incremental = false
-opt-level = "z"
-
-[features]
-constructor = []
-constructor-test = []"#,
-            project_name, project_name
-        ),
-    )
-    .await
-    .context("failed to init Cargo.toml")?;
+    let cargo_content = match mode {
+        Mode::Rusty => format!(rusty_cargo_template!(), project_name, project_name),
+        _ => format!(default_cargo_template!(), project_name, project_name),
+    };
+    write("Cargo.toml", cargo_content)
+        .await
+        .context("failed to init Cargo.toml")?;
     Ok(())
 }
 
@@ -105,42 +78,11 @@ async fn init_lib_file(mode: &Mode) -> Result<()> {
     create_dir(Path::new("./src"))
         .await
         .context("failed to create src folder")?;
-    write(
-        "./src/lib.rs",
-        r#"
-use sewup_derive::{ewasm_constructor, ewasm_fn, ewasm_fn_sig, ewasm_main, ewasm_test};
-
-#[ewasm_constructor]
-fn constructor() {}
-
-#[ewasm_fn]
-fn hello() -> anyhow::Result<String> {
-    Ok("hello world".to_string())
-}
-
-#[ewasm_main(auto)]
-fn main() -> anyhow::Result<String> {
-    let contract = sewup::primitives::Contract::new()?;
-    let greeting = match contract.get_function_selector()? {
-        ewasm_fn_sig!(hello) => hello()?,
-        _ => panic!("unknown handle"),
+    let task = match mode {
+        Mode::Rusty => write("./src/lib.rs", RUSTY_CONTRACT),
+        _ => write("./src/lib.rs", DEFAULT_CONTRACT),
     };
-    Ok(greeting)
-}
-
-#[ewasm_test]
-mod tests {
-    use super::*;
-    use sewup_derive::{ewasm_assert_eq, ewasm_auto_assert_eq, ewasm_output_from};
-
-    #[ewasm_test]
-    fn test_get_greeting() {
-        ewasm_auto_assert_eq!(hello(), "hello world".to_string());
-    }
-}"#,
-    )
-    .await
-    .context("failed to create sample code")?;
+    task.await.context("failed to create sample code")?;
     Ok(())
 }
 
