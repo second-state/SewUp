1220d0731f8ccb29b28fb86c791113d10ba0852e
Antonio Yang <yanganto@gmail.com>
2021-11-02 10:05:33 +0800

    Provide default template for default mode

--- cargo-sewup/src/constants.rs
+++ cargo-sewup/src/constants.rs
@@ -58,24 +58,27 @@ fn hello() -> anyhow::Result<String> {
     Ok("hello world".to_string())
 }
 
-#[ewasm_main(auto)]
-fn main() -> anyhow::Result<String> {
+#[ewasm_main]
+fn main() -> anyhow::Result<()> {
     let contract = sewup::primitives::Contract::new()?;
-    let greeting = match contract.get_function_selector()? {
+    match contract.get_function_selector()? {
         ewasm_fn_sig!(hello) => hello()?,
         _ => panic!("unknown handle"),
     };
-    Ok(greeting)
+    Ok(())
 }
 
 #[ewasm_test]
 mod tests {
     use super::*;
-    use sewup_derive::{ewasm_assert_eq, ewasm_auto_assert_eq, ewasm_output_from};
+    use sewup_derive::{ewasm_assert_eq, ewasm_assert_ok};
 
     #[ewasm_test]
     fn test_get_greeting() {
-        ewasm_auto_assert_eq!(hello(), "hello world".to_string());
+        // The default mode does not use anything successful return from Rust
+        // The only thing is return ok or not
+        ewasm_assert_eq!(hello(), vec![]);
+        ewasm_assert_ok!(hello());
     }
 }"#;
 
