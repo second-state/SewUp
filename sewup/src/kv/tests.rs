2d6f25176585629ee168676bb89e6db31f506f64
Antonio Yang <yanganto@gmail.com>
2022-01-01 20:55:14 +0800

    Implement pop_back for kv

--- sewup/src/kv/tests.rs
+++ sewup/src/kv/tests.rs
@@ -38,3 +38,21 @@ fn test_pop_for_bucket() {
     assert_eq!(bucket.get(2).unwrap(), None);
     assert_eq!(bucket.get(3).unwrap(), Some(3));
 }
+
+#[cfg(feature = "default")]
+#[test]
+fn test_pop_back_for_bucket() {
+    let mut bucket = Bucket {
+        name: "test_bucket".into(),
+        raw_bucket: (vec![], vec![]),
+        phantom_k: PhantomData::<usize>,
+        phantom_v: PhantomData::<usize>,
+    };
+    bucket.set(1, 1);
+    bucket.set(2, 2);
+    assert_eq!(bucket.get(1).unwrap(), Some(1));
+    let last = bucket.pop_back();
+    assert_eq!(last, Some((2, 2)));
+    assert_eq!(bucket.get(1).unwrap(), Some(1));
+    assert_eq!(bucket.get(2).unwrap(), None);
+}
